df1cb11d6d560f8ca4b1b55700b56d4fa3a013e0
Alex <64134232+LookOlga@users.noreply.github.com>
2022-08-22 17:53:01 +0300

    Updates

--- plugins/highlightPlugin.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import 'highlight.js/styles/sunburst.css';
-import hljsVuePlugin from '@highlightjs/vue-plugin';
-import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-
-
-hljs.registerLanguage('javascript', javascript);
-Vue.use(hljsVuePlugin);
--- plugins/iconLoader.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import Icon from '~/components/Icons/Icon';
-
-Vue.component('Icon', Icon);
